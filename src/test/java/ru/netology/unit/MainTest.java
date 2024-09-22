e2621c1bdc5440b52df632212e4a55deb756bc8b
Любовь <altukhovalv@yandex.ru>
2024-09-22 17:25:19 +0300

    add

---
+++ src/test/java/ru/netology/unit/MainTest.java
@@ -0,0 +1,29 @@
+package ru.netology.unit;
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+class MainTest {
+
+    @Test
+    void main() {
+        // Given - When - Then
+// Предусловия
+                given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello World") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hello World"))
+                        .contentType(ContentType.JSON)
+        ;
+    }
+}
